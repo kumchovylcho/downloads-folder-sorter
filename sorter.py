e0970dfc209cb97856dd12e9a43dd65cf1edce28
Tamer <tamerchoyy@gmail.com>
2023-11-22 00:47:45 +0200

    created main function

--- sorter.py
+++ sorter.py
@@ -12,23 +12,27 @@ def is_dir(path_to_dir: str):
     return os.path.isdir(path_to_dir)
 
 
-base_path = os.path.join(os.environ['USERPROFILE'], 'Downloads')
+def sorter():
+    base_path = os.path.join(os.environ['USERPROFILE'], 'Downloads')
 
-all_files = os.listdir(base_path)
-for file in all_files:
-    current_file_path = os.path.join(base_path, file)
+    all_files = os.listdir(base_path)
+    for file in all_files:
+        current_file_path = os.path.join(base_path, file)
 
-    if is_dir(current_file_path):
-        continue
+        if is_dir(current_file_path):
+            continue
 
-    file_extension = file.split(".")[-1]
-    if file_extension not in extensions:
-        continue
+        file_extension = file.split(".")[-1]
+        if file_extension not in extensions:
+            continue
 
-    directory_path = os.path.join(base_path, file_extension)
-    if not dir_exists(directory_path):
-        os.mkdir(directory_path)
+        directory_path = os.path.join(base_path, file_extension)
+        if not dir_exists(directory_path):
+            os.mkdir(directory_path)
 
-    shutil.move(src=current_file_path,
-                dst=directory_path
-                )
+        shutil.move(src=current_file_path,
+                    dst=directory_path
+                    )
+
+
+sorter()
